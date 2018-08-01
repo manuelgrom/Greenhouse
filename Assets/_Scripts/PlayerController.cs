890919133b30c4e5b65d846c69912388d0651507
Manuel Grom <grom.manuel@gmail.com>
2018-08-01 17:23:27 +0200

    movement

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -5,7 +5,13 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     #region PublicVar
-    public float runSpeed = 0.0f;
+    public float runSpeed;
+    public float walkSpeed;
+
+    //Jumping
+    public LayerMask groundLayer;
+    public Transform groundCheck;
+    public float jumpHeight;
     #endregion PublicVar
 
     #region PrivateVar
@@ -13,7 +19,14 @@ public class PlayerController : MonoBehaviour
     private Animator m_myAnimator;
     private float m_move;
     private bool m_facingRight;
-    private Vector3 m_myScale; 
+    private Vector3 m_myScale;
+    private float m_sneaking;
+
+    //for jumping
+    private bool m_grounded;
+    private Collider[] m_groundCollisions;
+    private float m_groundCheckRadius = 0.2f;
+
     #endregion PrivateVar
 
     // Use this for initialization
@@ -24,6 +37,8 @@ public class PlayerController : MonoBehaviour
         m_move = 0.0f;
         m_facingRight = true;
         m_myScale = Vector3.zero;
+        m_sneaking = 0.0f;
+        m_grounded = false;
     }
 
     // Update is called once per frame (rate dependent)
@@ -34,13 +49,49 @@ public class PlayerController : MonoBehaviour
     //fixed rate after physic engin is run! Its constant
     private void FixedUpdate()
     {
+        //jump
+        if (m_grounded && Input.GetAxis("Jump") > 0)
+        {
+            m_grounded = false;
+            m_myAnimator.SetBool("grounded", m_grounded);
+            m_myRB.AddForce(new Vector3(0, jumpHeight, 0)); //actual jump
+            Debug.Log("Space bar pressed");
+        }
+        
+        //Jump collision check
+        m_groundCollisions = Physics.OverlapSphere(groundCheck.position, m_groundCheckRadius, groundLayer);
+        if (m_groundCollisions.Length > 0)
+        {
+            m_grounded = true;
+            Debug.Log("is grounded");
+        }
+        else
+        {
+            m_grounded = false;
+            Debug.Log("is not  grounded");
+
+        }
+
+        m_myAnimator.SetBool("grounded", m_grounded);
+
         // Keys A = -1 Key D = 1
         m_move = Input.GetAxis("Horizontal");
         m_myAnimator.SetFloat("speed", Mathf.Abs(m_move));
 
-        //movement
-        m_myRB.velocity = new Vector3(m_move * runSpeed, m_myRB.velocity.y, 0);
+        //Sneaking /Fire3 = left shift button in unity input settings 
+        m_sneaking = Input.GetAxisRaw("Fire3");
+        m_myAnimator.SetFloat("sneaking", m_sneaking);
 
+        //movement
+        if (m_sneaking > 0 && m_grounded) //shift is pressed
+        {
+            m_myRB.velocity = new Vector3(m_move * walkSpeed, m_myRB.velocity.y, 0);
+        }
+        else //shift not pressed 
+        {
+            m_myRB.velocity = new Vector3(m_move * runSpeed, m_myRB.velocity.y, 0);
+        }
+        //Flipping Character
         if (m_move > 0 && !m_facingRight)
         {
             Flip();
@@ -49,7 +100,9 @@ public class PlayerController : MonoBehaviour
         {
             Flip();
         }
+
     }
+
     //Flipping the Character L/R 
     private void Flip()
     {
